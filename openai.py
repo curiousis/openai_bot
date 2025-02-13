6290112ad32611f6cd1d038a59e438db80a9b250
isra <sb15286@student.samk.fi>
2025-02-13 13:59:39 +0200

    first commit

---
+++ openai.py
@@ -0,0 +1,45 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+key = os.getenv("API_KEY")
+
+max_completion_tokens = 50
+temperature = 1
+
+old_data = []
+
+
+def main():
+    while True:
+        user_input = input("Input: ")
+        response = chatgpt_query(user_input)
+        print(print(response["choices"][0]["message"]["content"]))
+
+
+def chatgpt_query(prompt):
+    headers = {"Authorization": "Bearer " + key, "Content-Type": "application/json"}
+    data = {
+        "messages": [{"role": "user", "content": prompt}],
+        "max_completion_tokens": max_completion_tokens,
+        "temperature": temperature,
+        "model": "gpt-4o",
+    }
+    old_data.append({"role": "user", "content": prompt})
+    data["messages"] = old_data
+    r = requests.post(
+        "https://api.openai.com/v1/chat/completions",
+        headers=headers,
+        data=json.dumps(data),
+    )
+
+    r_json = r.json()
+    return r_json
+
+
+if __name__ == "__main__":
+    main()
